83c4ae4f293d23fbbdce54ef8697667f75dd328a
im-ant <anthony.gx.chen@gmail.com>
2019-03-05 14:19:25 -0500

    Added human and computer game types and different verbosity settings

--- SimuGame.py
+++ SimuGame.py
@@ -8,11 +8,21 @@ import Agent as Ag
 print('Four-In-A-Row')
 print()
 
-## Default board dimensions (also need to change the API)##
+## Default board dimensions (also need to change the API) ##
 BOARDWIDTH = 7
 BOARDHEIGHT = 6
 
-## Default Agent play ##
+## Game Setting ##
+# 'Human_v_Agent' or 'Agent_v_Computer'
+GameType = 'Human_v_Agent'
+
+
+## Agent Settings ##
+# How many iteration of MCTS to run per move
+MCTS_ITERS = 500
+# Verbosity of agent and game states
+VERBOSITY = True
+# Default tile when playing against computer
 DE_AGENT_TILE = 'O'
 
 
@@ -29,14 +39,14 @@ def agentDefaultPlay(verbose=True):
     turn = 'human' if DE_AGENT_TILE=='O' else 'computer'
 
     # Initialize Agent
-    Agent = Ag.CFAgent(humanTile, computerTile, BOARDWIDTH, BOARDHEIGHT)
+    Agent = Ag.CFAgent(humanTile, computerTile, BOARDWIDTH, BOARDHEIGHT, mctsIters=MCTS_ITERS)
 
     ## Start game ##
     while True:
         if turn == 'human':
             if verbose:
                 C4.drawBoard(mainBoard)
-            move = Agent.decideMove(mainBoard)
+            move = Agent.decideMove(mainBoard, verbose=verbose)
             C4.makeMove(mainBoard, humanTile, move)
             if C4.isWinner(mainBoard, humanTile):
                 winner = 'agent'
@@ -65,16 +75,16 @@ def agentDefaultPlay(verbose=True):
 ###
 # Agent against human play
 ###
-def agentHumanPlay():
+def agentHumanPlay(verbose=True):
     ## Initialization ##
     # Decide who to go first
     humanTile, computerTile = C4.enterHumanTile()
-    turn = C4.whoGoesFirst()
+    turn = 'human' if humanTile=='O' else 'computer'
     print('The %s player will go first.' % (turn))
     mainBoard = C4.getNewBoard()
 
     # Initialize agent
-    Agent = Ag.CFAgent(computerTile, humanTile, BOARDWIDTH, BOARDHEIGHT)
+    Agent = Ag.CFAgent(computerTile, humanTile, BOARDWIDTH, BOARDHEIGHT, mctsIters=MCTS_ITERS)
 
     ## Start game ##
     while True:
@@ -89,7 +99,7 @@ def agentHumanPlay():
         else:
             C4.drawBoard(mainBoard)
             print('The agent is thinking...')
-            move = Agent.decideMove(mainBoard)
+            move = Agent.decideMove(mainBoard, verbose=verbose)
             C4.makeMove(mainBoard, computerTile, move)
             if C4.isWinner(mainBoard, computerTile):
                 winner = 'agent'
@@ -108,49 +118,11 @@ def agentHumanPlay():
 
 
 def main():
-    #agentDefaultPlay(verbose=True)
-    agentHumanPlay()
-
-
-
-
-
-
-'''
-Agent = Ag.CFAgent('O', 'X', BOARDWIDTH, BOARDHEIGHT)
-
-
-### Initialize game ###
-mainBoard = C4.getNewBoard()
-humanTile, computerTile = 'O', 'X'
-turn = 'human'
-
-while True:
-    if turn == 'human':
-        C4.drawBoard(mainBoard)
-        move = Agent.decideMove(mainBoard)
-        C4.makeMove(mainBoard, humanTile, move)
-        if C4.isWinner(mainBoard, humanTile):
-            winner = 'human'
-            break
-        turn = 'computer'
-    else:
-        C4.drawBoard(mainBoard)
-        print('The computer is thinking...')
-        move = C4.getComputerMove(mainBoard, computerTile)
-        C4.makeMove(mainBoard, computerTile, move)
-        if C4.isWinner(mainBoard, computerTile):
-            winner = 'computer'
-            break
-        turn = 'human'
-
-    if C4.isBoardFull(mainBoard):
-        winner = 'tie'
-        break
-
-C4.drawBoard(mainBoard)
-print('Winner is: %s' % winner)
-'''
+    # Human against agent
+    if GameType == 'Human_v_Agent':
+        agentHumanPlay(verbose=VERBOSITY)
+    elif GameType == 'Agent_v_Computer':
+        agentDefaultPlay(verbose=VERBOSITY)
 
 
 if __name__ == '__main__':
